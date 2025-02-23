c03babfe24fc0712a7f2edc807415dbceb3e0f94
David Moreno Cerezo <131790198+DavidMoCe@users.noreply.github.com>
2025-02-23 19:58:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,8 @@ with tab3:
     w_2 = st.slider("Peso w_2", 0.0, 5.0, step=0.5, key="w2_tab3")
     x_2 = st.number_input("Entrada x_2", key="x2_tab3")
 
+    sesgo = st.number_input("Sesgo/Bias", key="sesgo" value=10)
+
     resultado = x_0 * w_0 + x_1 * w_1 + x_2 * w_2 + 10
     if st.button("Calcular", key="boton_2"):
         st.write("La salida de la neurona es", resultado)
